46324631aff85ecaca3b76567c262c8d4d27ae20
Sébastien Mosser <sebastien.mosser@gmail.com>
2012-06-26 15:04:03 +0200

    Mapping repository declared as a portal artifact

--- net.modelbased.mediation.service.repository.mapping/src/main/scala/net/modelbased/mediation/service/repository/mapping/MappingRepositoryService.scala
+++ net.modelbased.mediation.service.repository.mapping/src/main/scala/net/modelbased/mediation/service/repository/mapping/MappingRepositoryService.scala
@@ -4,7 +4,7 @@
  * Copyright (C) 2012-  SINTEF ICT
  * Contact: Franck Chauvel <franck.chauvel@sintef.no>
  *
- * Module: net.modelbased.mediation.service.repository.model
+ * Module: net.modelbased.mediation.service.repository.mapping
  *
  * SensApp is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
