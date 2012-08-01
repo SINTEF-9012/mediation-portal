e77ac9f06cdfd82a5bb46765569aba411da0e63c
Sébastien Mosser <sebastien.mosser@gmail.com>
2012-08-01 14:37:03 +0200

    new license header

--- net.modelbased.mediation.library.algorithm/src/main/scala/net/modelbased/mediation/library/algorithm/mof/printer/MofPrinter.scala
+++ net.modelbased.mediation.library.algorithm/src/main/scala/net/modelbased/mediation/library/algorithm/mof/printer/MofPrinter.scala
@@ -1,23 +1,23 @@
 /**
- * This file is part of SensApp [ http://sensapp.modelbased.net ]
+ * This file is part of Mediation Portal [ http://mosser.github.com/mediation-portal ]
  *
  * Copyright (C) 2010-  SINTEF ICT
  * Contact: Franck Chauvel <franck.chauvel@sintef.no>
  *
  * Module: net.modelbased.mediation.library.algorithm
  *
- * SensApp is free software: you can redistribute it and/or modify
+ * Mediation Portal is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of
  * the License, or (at your option) any later version.
  *
- * SensApp is distributed in the hope that it will be useful, but
+ * Mediation Portal is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General
- * Public License along with SensApp. If not, see
+ * Public License along with Mediation Portal. If not, see
  * <http://www.gnu.org/licenses/>.
  */
 package net.modelbased.mediation.library.algorithm.mof.printer
