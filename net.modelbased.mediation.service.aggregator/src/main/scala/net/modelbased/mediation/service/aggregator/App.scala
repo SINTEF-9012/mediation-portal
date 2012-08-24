e8b6eb11d65a3c75fbb3fb5240d1d2e0c94e0359
Franck Chauvel <franck.chauvel@sintef.no>
2012-08-24 11:20:33 +0200

    Empty model aggregator service

---
+++ net.modelbased.mediation.service.aggregator/src/main/scala/net/modelbased/mediation/service/aggregator/App.scala
@@ -0,0 +1,37 @@
+/**
+ * This file is part of Mediation Portal [ http://mosser.github.com/mediation-portal ]
+ *
+ * Copyright (C) 2012-  SINTEF ICT
+ * Contact: Franck Chauvel <franck.chauvel@sintef.no>
+ *
+ * Module: net.modelbased.mediation.service.aggregator
+ *
+ * Mediation Portal is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * Mediation Portal is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with Mediation Portal. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+package net.modelbased.mediation.service.merger
+
+/**
+ * @author ${user.name}
+ */
+object App {
+  
+  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
+  
+  def main(args : Array[String]) {
+    println( "Hello World!" )
+    println("concat arguments = " + foo(args))
+  }
+
+}
