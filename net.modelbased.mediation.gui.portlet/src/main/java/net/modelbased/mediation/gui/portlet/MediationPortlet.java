9feeaee294755fe8c925827763f5b3e801e0457a
Franck Chauvel <franck.chauvel@sintef.no>
2012-10-23 13:53:58 +0200

    Minor bug fixes in the Javascript functions

--- net.modelbased.mediation.gui.portlet/src/main/java/net/modelbased/mediation/gui/portlet/MediationPortlet.java
+++ net.modelbased.mediation.gui.portlet/src/main/java/net/modelbased/mediation/gui/portlet/MediationPortlet.java
@@ -1,13 +1,15 @@
 package net.modelbased.mediation.gui.portlet;
- 
+
 import java.io.IOException;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 import javax.portlet.GenericPortlet;
-import javax.portlet.PortletConfig; 
+import javax.portlet.PortletConfig;
 import javax.portlet.PortletException;
 import javax.portlet.PortletRequestDispatcher;
+import javax.portlet.ProcessAction;
+import javax.portlet.ProcessEvent;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 import javax.portlet.Event;
@@ -15,10 +17,9 @@ import javax.portlet.EventRequest;
 import javax.portlet.EventResponse;
 import javax.xml.namespace.QName;
 
-
 /**
  * Implementation of the event processing supported by the Mediation portlet.
- * The portlet can basically receive a event named "mappingResult(mappingId)",
+ * The portlet can basically receive a event named "showMapping(mappingId)",
  * where mappingId is a simple string describing the unique ID of the mapping to
  * display, and send the event "mappingComplete(mappingId)" when the user
  * considers the mapping as complete
@@ -28,57 +29,66 @@ import javax.xml.namespace.QName;
  * @since 0.0.1
  */
 public class MediationPortlet extends GenericPortlet {
-
 	
-	private static final String NORMAL_VIEW = "/view.jsp";
-	private PortletRequestDispatcher normalView;
-
-	private String message = null;
 	
-	public static final QName SHOW_MAPPING = new QName("http://www.modelbased.net", "showMapping");
-	public static final QName MAPPING_COMPLETE = new QName("http://www.modelbased.net", "mappingComplete");
-
-	public static final String MESSAGE_ATTR = "MESSAGE";
-
-	private int counter = 0;
+	private static final String VIEW_MODE_PAGE = "/view.jsp";
 	
-	public void doView(RenderRequest request, RenderResponse response) throws PortletException,
-			IOException
-	{		
-		if (message !=null)
-			request.setAttribute(MESSAGE_ATTR, "Message(" + ++counter + ")" + message);
-		
-		normalView.include(request, response);
-		
-		message = null;
-	}
+	private PortletRequestDispatcher viewMode;
 
-	public void init(PortletConfig config) throws PortletException
-	{
+	public static final QName SHOW_MAPPING = new QName(
+			"http://www.modelbased.net", "showMapping");
+	
+	public static final QName MAPPING_COMPLETE = new QName(
+			"http://www.modelbased.net", "mappingComplete");
+
+
+
+	/*
+	 * (non-Javadoc)
+	 * @see javax.portlet.GenericPortlet#init(javax.portlet.PortletConfig)
+	 */
+	public void init(PortletConfig config) throws PortletException {
 		super.init(config);
-		normalView = config.getPortletContext().getRequestDispatcher(NORMAL_VIEW);
+		viewMode = config.getPortletContext().getRequestDispatcher(
+				VIEW_MODE_PAGE);
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see javax.portlet.GenericPortlet#doView(javax.portlet.RenderRequest, javax.portlet.RenderResponse)
+	 */
+	public void doView(RenderRequest request, RenderResponse response)
+			throws PortletException, IOException {		
+		viewMode.include(request, response);
 	}
 
-	public void destroy()
-	{
-		normalView = null;
+
+	/*
+	 * (non-Javadoc)
+	 * @see javax.portlet.GenericPortlet#destroy()
+	 */
+	public void destroy() {
+		viewMode = null;
 		super.destroy();
 	}
-	
-	public void processAction(ActionRequest request, ActionResponse response)
-			throws PortletException, IOException
-	{		
-		response.setEvent(MediationPortlet.MAPPING_COMPLETE, "Hello!");
+
+	/**
+	 * Event handler to handle the external "showMapping" event (sent by the
+	 * composition portlet).
+	 * 
+	 */
+	@ProcessEvent(qname = "{http://www.modelbased.net}showMapping")
+	public void showMapping(EventRequest request, EventResponse response) {
+		Event event = request.getEvent();
+		System.out.println(">>>>>>>>>>>>>>>>>>>>>" + event.getValue());
 	}
 
-	public void processEvent(EventRequest eventRequest, EventResponse response)
-			throws PortletException, IOException
-	{
-		Event event = eventRequest.getEvent();
-		if (event.getQName().equals(MediationPortlet.SHOW_MAPPING)) {
-			message = (String) event.getValue();
-		}			
+	/**
+	 * Trigger the mapping complete action
+	 */
+	@ProcessAction(name = "mappingComplete")
+	public void mappingComplete(ActionRequest request, ActionResponse response) {
+		response.setEvent(MediationPortlet.MAPPING_COMPLETE, "Hello!");
 	}
-	
 
 }
--- net.modelbased.mediation.gui.portlet/src/main/webapp/js/mediation.js
+++ net.modelbased.mediation.gui.portlet/src/main/webapp/js/mediation.js
@@ -6,33 +6,18 @@
  *
  */
 
-var MediationEvents = {};
-
-
-MediationEvents.listeners = new Array(); 
-
-MediationEvents.addListener = function(listener) {
-	MediationEvents.listeners.push(listener); 
-};
-
-MediationEvents.invokeListeners = function(message) {
-	for (var i = 0; i < MediationEvents.listeners.length; i++) {
-		var callback = MediationEvents.listeners[i]; 
-		callback(message); 
-	}
-};
-
 
 /**
- * Function trigger when the user press the Mapping Complete button. It 
- * returns the focus to the composition portal
+ * Function triggered when the user press the Mapping Complete button. It returns
+ * the focus to the composition portal
  * 
  */
 function doMappingComplete() {
-	var uid = 	$("#mapping-id").value;
-	var confirmed =	confirm("Are you sure that this mapping is complete'?");
+	var uid = $("#mapping-id").value;
+	var confirmed = confirm("Are you sure that this mapping is complete'?");
 	if (confirmed) {
-		MediationEvents.invokeListeners(uid);
+		var event = { uid: uid }; 
+		Liferay.fire("mappingComplete", event);
 	}
 }
 
@@ -42,112 +27,127 @@ function doMappingComplete() {
  * portlet. The mapping is displayed as and the title page is updated so as to
  * show the ID of the mapping.
  * 
- * @param sender
- *            the string identifying the sender of the messages
- * 
- * @param mId
- *            the unique ID identifing the mapping within the mapping repository
- * 
+ * @param event the event that triggered this callback 
  */
-var loadMapping = function(mId) {
-	alert("Loading mapping '" + mId + "'!");
-	$("#mapping-id").text(mId); // Update the title of the page
-	fetchMappingEntriesOf(mId, function(json) {
+var doLoadMapping = function(event) {
+	var uid = event.details[0].uid;
+	alert("Loading mapping '" + uid + "'!");
+	$("#mapping-id").text(uid); // Update the title of the page
+	fetchMappingEntriesOf(uid, function(json) {
 		var table = $("#results").dataTable();
 		table.fnClearTable();
 		table.fnAddData(json);
 	});
 };
 
-// we have to wait until the DOM is loaded, otherwise MyEvents namespace is not
-// ensured to exist
-var localOnLoad = function() {
-	CompositionEvents.addListener(loadMapping);
-};
 
-
-var currentOnload = window.onload;
-if (currentOnload != undefined) {
-	if(typeof(currentOnload) == "function"){
-		window.onload = function() {
-			currentOnload();
-			localOnLoad();
-		};
-	} else {
-		window.onload = localOnLoad;
-	};
-} else {
-	window.onload = localOnLoad;
-};
-
-
-/*
- * MAIN PROGRAM Bind various functions to pages event
+/**
+ * Bind the "loadMapping(uid) event to the Javascript function doLoadMapping
  */
-
-$(document).ajaxStop(function() { 
-
-});
+Liferay.on("showMapping", doLoadMapping);
 
 
-$(document)
-		.ready(function() {
-			activateBackEnd("SINTEF-DEMO");
-			$('#results')
-					.dataTable({
-					"sPaginationType" : "bootstrap",
-					"aaData" : [],
-					"aoColumns" : [ {
-					"sTitle" : "Source Element",
-					"mDataProp" : "source"
-					}, {
-					"sTitle" : "Target Element",
-					"mDataProp" : "target"
-					}, {
-					"sTitle" : "Degree",
-					"mDataProp" : "degree",
-					"sClass" : "center",
-					"sWidth" : "100px",
-					"fnRender" : function(object) {
-						return (object.aData.degree * 100).toFixed(2);
-					},
-					"bUseRendered" : false
-					}, {
-					"sTitle" : "Origin",
-					"mDataProp" : "origin"
-					}, {
-					"sTitle" : "Confirmation?",
-					"mDataProp" : "isValidated",
-					"fnRender" : function(object) {
 
-						var status = {
-						unknown : "",
-						approved : "",
-						disapproved : ""
-						};
-						if (object.aData["isValidated"] == null) {
-							status["unknown"] = "active";
-						} else if (object.aData["isValidated"] == false) {
-							status["disapproved"] = "active";
-						} else {
-							status["approved"] = "active";
-						}
+Liferay.Portlet.ready(
+	    function(portletId, node) {
+				activateBackEnd("SINTEF-DEMO");
+				$('#results').dataTable(
+					{
+									"sPaginationType" : "bootstrap",
+									"aaData" : [],
+									"aoColumns" : [
+											{
+												"sTitle" : "Source Element",
+												"mDataProp" : "source"
+											},
+											{
+												"sTitle" : "Target Element",
+												"mDataProp" : "target"
+											},
+											{
+												"sTitle" : "Degree",
+												"mDataProp" : "degree",
+												"sClass" : "center",
+												"sWidth" : "100px",
+												"fnRender" : function(
+														object) {
+													return (object.aData.degree * 100)
+															.toFixed(2);
+												},
+												"bUseRendered" : false
+											},
+											{
+												"sTitle" : "Origin",
+												"mDataProp" : "origin"
+											},
+											{
+												"sTitle" : "Confirmation?",
+												"mDataProp" : "isValidated",
+												"fnRender" : function(
+														object) {
 
-						var callback = "function (data) { alert(\'Status update successful!\'); } ";
-						var uid = $("#mapping-id")[0].innerHTML;
-						var source = object.aData.source;
-						var target = object.aData.target;
+													var status = {
+														unknown : "",
+														approved : "",
+														disapproved : ""
+													};
+													if (object.aData["isValidated"] == null) {
+														status["unknown"] = "active";
+													} else if (object.aData["isValidated"] == false) {
+														status["disapproved"] = "active";
+													} else {
+														status["approved"] = "active";
+													}
 
-						var unknownButton = "<button class=\"btn btn-mini " + status["unknown"] + "\" onclick=\"setUnknown('" + uid + "', '" + source + "', '" + target + "', " + callback + ");\"><i class=\"icon-question-sign\"></i></button>";
-						var approveButton = "<button class=\"btn btn-mini " + status["approved"] + "\" onclick=\"approve('" + uid + "', '" + source + "', '" + target + "', " + callback + ");\"><i class=\"icon-thumbs-up\"></i></button>";
-						var disapproveButton = "<button class=\"btn btn-mini " + status["disapproved"] + "\" onclick=\"disapprove('" + uid + "', '" + source + "', '" + target + "', " + callback + ");\"><i class=\"icon-thumbs-down\"></i></button>";
+													var callback = "function (data) { alert(\'Status update successful!\'); } ";
+													var uid = $("#mapping-id")[0].innerHTML;
+													var source = object.aData.source;
+													var target = object.aData.target;
 
-						return "<div class=\"btn-group\" data-toggle=\"buttons-radio\">" + unknownButton + approveButton + disapproveButton + "</div>";
-					},
-					"bUseRendered" : false
-					} ],
-					"oLanguage" : {
-						"sLengthMenu" : "_MENU_ records per page"
-					}
-					});
-		});
+													var unknownButton = "<button class=\"btn btn-mini "
+															+ status["unknown"]
+															+ "\" onclick=\"setUnknown('"
+															+ uid
+															+ "', '"
+															+ source
+															+ "', '"
+															+ target
+															+ "', "
+															+ callback
+															+ ");\"><i class=\"icon-question-sign\"></i></button>";
+													var approveButton = "<button class=\"btn btn-mini "
+															+ status["approved"]
+															+ "\" onclick=\"approve('"
+															+ uid
+															+ "', '"
+															+ source
+															+ "', '"
+															+ target
+															+ "', "
+															+ callback
+															+ ");\"><i class=\"icon-thumbs-up\"></i></button>";
+													var disapproveButton = "<button class=\"btn btn-mini "
+															+ status["disapproved"]
+															+ "\" onclick=\"disapprove('"
+															+ uid
+															+ "', '"
+															+ source
+															+ "', '"
+															+ target
+															+ "', "
+															+ callback
+															+ ");\"><i class=\"icon-thumbs-down\"></i></button>";
+
+													return "<div class=\"btn-group\" data-toggle=\"buttons-radio\">"
+															+ unknownButton
+															+ approveButton
+															+ disapproveButton
+															+ "</div>";
+												},
+												"bUseRendered" : false
+											} ],
+									"oLanguage" : {
+										"sLengthMenu" : "_MENU_ records per page"
+									}
+								});
+	    });
